ca95a78c7a04725b6fa0ff9724b1c20b84ad2297
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-04-27 12:13:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Digite o seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Olá " + nome + "!");
+
+Console.WriteLine("Qual a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine($"{nome} você tem {idade} anos");
+
